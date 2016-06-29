63b3a09afda5aeb81d02b8b07c7922145386913d
Oscar <oscarshen09@foxmail.com>
2016-06-29 21:51:54 +0800

    echo socket final

--- src/echoSocket/EchoServer.java
+++ src/echoSocket/EchoServer.java
@@ -1,14 +1,27 @@
 package echoSocket;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+/**
+ * echo服务器端
+ * 
+ * @author Oscar
+ *
+ */
 public class EchoServer {
 	private int port = 8000;
 	private ServerSocket serverSocket;
+
+	public static void main(String[] args) throws IOException {
+		new EchoServer().service();
+	}
 
 	public EchoServer() throws IOException {
 		serverSocket = new ServerSocket(port);
@@ -22,5 +35,40 @@ public class EchoServer {
 	private PrintWriter getWriter(Socket socket) throws IOException {
 		OutputStream socketOut = socket.getOutputStream();
 		return new PrintWriter(socketOut, true);
+	}
+
+	private BufferedReader getReader(Socket socket) throws IOException {
+		InputStream socketIn = socket.getInputStream();
+		return new BufferedReader(new InputStreamReader(socketIn));
+	}
+
+	public void service() {
+		while (true) {
+			Socket socket = null;
+			try {
+				socket = serverSocket.accept();
+				System.out.println("New connection accepted " + socket.getInetAddress() + ";" + socket.getPort());
+				BufferedReader br = getReader(socket);
+				PrintWriter pw = getWriter(socket);
+
+				String msg = null;
+				while ((msg = br.readLine()) != null) {
+					System.out.println(msg);
+					pw.println(echo(msg));
+					if (msg.equals("bye")) {
+						break;
+					}
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					if (socket != null)
+						socket.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 }
