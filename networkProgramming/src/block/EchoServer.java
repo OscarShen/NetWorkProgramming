1c94e262e002d22e8bb0bba0d6e77ef38e8dc847
ruiyao.shen <oscarshen09@foxmail.com>
2016-07-12 20:53:41 +0800

    socket channel for echo server in block

---
+++ networkProgramming/src/block/EchoServer.java
@@ -0,0 +1,86 @@
+package block;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * 使用nio实现阻塞方式EchoServer
+ * 
+ * @author ruiyao.shen
+ *
+ */
+public class EchoServer {
+	private int port = 8000;
+	private ServerSocketChannel serverSocketChannel = null;
+	private ExecutorService executorService;// 线程池
+	private static final int POOL_SIZE = 4;// 线程池中的工作线程数目
+
+	public EchoServer() throws IOException {
+		// 创建一个线程池
+		executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors() * POOL_SIZE);
+		// 创建一个ServerSocketChannel对象
+		serverSocketChannel = ServerSocketChannel.open();
+		// 保证关闭服务器程序后可以马上再次启动服务器程序
+		serverSocketChannel.socket().setReuseAddress(true);
+		// 把服务器进程与一个本地端口绑定
+		serverSocketChannel.socket().bind(new InetSocketAddress(port));
+		System.out.println("服务器启动");
+	}
+
+	public void service() {
+		while (true) {
+			SocketChannel socketChannel = null;
+			try {
+				socketChannel = serverSocketChannel.accept();
+				executorService.execute(new Handler(socketChannel));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		new EchoServer().service();
+	}
+
+	class Handler implements Runnable {
+		private SocketChannel socketChannel;
+
+		public Handler(SocketChannel socketChannel) {
+			this.socketChannel = socketChannel;
+		}
+
+		@Override
+		public void run() {
+			handle(socketChannel);
+		}
+
+		public void handle(SocketChannel socketChannel) {
+			Socket socket = socketChannel.socket();
+			System.out.println("接收到客户连接，来自：" + socket.getInetAddress() + ":" + socket.getPort());
+			
+			
+		}
+		
+		private PrintWriter getWriter(Socket socket) throws IOException{
+			OutputStream os = socket.getOutputStream();
+			return new PrintWriter(new OutputStreamWriter(os));
+		}
+
+		private BufferedReader getReader(Socket socket) throws IOException{
+			InputStream is = socket.getInputStream();
+			return new BufferedReader(new InputStreamReader(is));
+		}
+	}
+}
